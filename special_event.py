111e50d05d6be3a7c785842a9984fbccf53f1d75
rashedaljaeedi <161239148+rashedaljaeedi@users.noreply.github.com>
2024-03-30 04:12:50 +0400

    Update special_event.py

--- special_event.py
+++ special_event.py
@@ -3,16 +3,18 @@ from exhibition import Exhibition  # Importing the Exhibition class from exhibit
 class SpecialEvent(Exhibition):
     def __init__(self, title, duration, location, purpose):
         super().__init__(title, duration, location)  # Calling the constructor of the superclass Exhibition
-        self.purpose = purpose  # Attribute to hold the purpose of the special event
-
-    # Method to represent SpecialEvent object as a string
-    def __str__(self):
-        return f"\n the  {self.title}  and the event takes place at  {self.duration}, and it located at {self.location} and the {self.purpose} for kids with cancer"
+        self.__purpose = purpose  # Attribute to hold the purpose of the special event
 
     # Getter for purpose
     def get_purpose(self):
-        return self.purpose
+        return self.__purpose
 
     # Setter for purpose
     def set_purpose(self, purpose):
-        self.purpose = purpose
+        self.__purpose = purpose
+
+    # Method to represent SpecialEvent object as a string
+    def __str__(self):
+        return f"\n the  {self.__title}  and the event takes place at  {self.__duration}, and it located at {self.__location} and the {self.__purpose} for kids with cancer"
+
+    
